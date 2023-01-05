974d670e6385397e1f9922760b237329ec34575f
thien2101 <thienb1910300@student.ctu.edu.vn>
2023-01-05 16:51:50 +0700

    khoi tao

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 const express = require("express")
 const cors = require("cors")
-
+const contactSRouter = require("./app/routes/contact.route")
 const app = express()
 
 app.use(cors());
 app.use(express.json());
+app.use("/api/contacts", contactSRouter)
 app.get("/", (req, res) => {
     res.json({
         message: "xin chao"
